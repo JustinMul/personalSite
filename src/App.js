5b9bf04ac42bd6d909abf4844eda559d451d7d6c
Justin Mulroney <justin@theryna.com>
2023-04-10 17:24:12 -0400

    add more styling to home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
+import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import cakeMan from './imgs/cakeMan.svg'
 function App() {
   
-  const handleClick=()=>{
+  const [buttonStatus, setButtonStatus] = useState(0)
+
+  const handleClick=(e)=>{
     console.log('this is clicked')
   }
+
+
 
   return (
     // note want to change the buttons to be red when showing what tab they're in
@@ -17,9 +22,9 @@ function App() {
         <div>Justin Mulroney</div>
         <div className='buttonContainer'>
           <Button text="Home" onClick={handleClick}></Button>
-          <Button text="About"></Button>
-          <Button text='Contact'></Button>
-          <Button text='Projects'></Button>
+          <Button text="About" onClick={handleClick}></Button>
+          <Button text='Contact' onClick={handleClick}></Button>
+          <Button text='Projects' onClick={handleClick}></Button>
         </div>
       </header>
 
@@ -31,11 +36,10 @@ function App() {
           <p className='nameIntro'>
             Hi, I'm Software Developer
           </p>
-          <p className='body'>
+          <p className='textBody'>
           I'm a passionate software developer with a keen eye for detail and a love for clean, efficient code. I specialize in Web Development,
            and have a proven track record of delivering high-quality solutions that meet my clients' needs. 
-           
-           When I'm not coding, you can find me [Your Hobbies or Interests]. Thanks for stopping by my site, and feel free to shoot me and email at Justinmulroney@gmail.com.
+           When I'm not coding, you can find me playing board games or bouldering. Thanks for stopping by my site, and feel free to shoot me and email at Justinmulroney@gmail.com.
           </p>
           <Button text="Hire Me"></Button>
         </div>
